1318006d1b61628157352f6ca538e6f09420b9a5
af-nh <ahmadfadel.20.6.11.2004@gmail.com>
2026-08-27 09:38:20 +0300

    branch 1 change

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Books - BookStore')
 
 @section('content')
-    <h2 class="text-3xl font-bold text-gray-800 mb-6">Books We Have in the second branch </h2>
+    <h2 class="text-3xl font-bold text-gray-800 mb-6">Books We Have in the first branch </h2>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach ($books as $book)
